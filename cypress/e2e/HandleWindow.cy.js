0358dca668585ae10841b875feabee19b1a2586b
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-03-05 23:06:24 +0300

    window handles

---
+++ cypress/e2e/HandleWindow.cy.js
@@ -0,0 +1,42 @@
+
+describe("Handling window",()=>{
+
+
+    it("Approach1 for handling windows",()=>{
+
+        cy.visit("https://the-internet.herokuapp.com/windows")
+        cy.get(".example >a").invoke("removeAttr","target").click()//target attribute is removed in order to open the 
+        //new window on the same window
+        //now we are on a new tab
+        let newWinHeading=cy.get(".example >h3")
+        newWinHeading.should("be.visible")
+        let expectedWinHeadingText="New Window"
+        newWinHeading.then((n)=>{
+           let actualText = n.text()
+
+            expect(actualText).to.eq(expectedWinHeadingText)
+            assert.equal(actualText,expectedWinHeadingText)
+
+        })
+
+        cy.wait(2000)
+
+        cy.go('back')//back to the previous window
+
+       var mainPageHeading= cy.get(".example>h3")
+       mainPageHeading.should("be.visible")
+
+       mainPageHeading.then((n)=>{
+        assert(n.text(),"Opening a new window")
+       })
+
+        
+
+
+
+    })
+
+
+
+
+})
