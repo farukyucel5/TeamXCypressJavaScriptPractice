f35c53f5de6657d18fe58cc682c8797b72b35b84
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-03-16 17:47:39 +0300

    Faruk 15.03.2023 updating

--- cypress/e2e/IFrames.cy.js
+++ cypress/e2e/IFrames.cy.js
@@ -1,24 +1,28 @@
 describe("handling IFrames",() => {
 
-    // beforeEach(() => {
-    //     cy.viewport(1280, 720)
+    beforeEach(() => {
+        cy.viewport(1280, 720)
     
-    //   })
+      })
   
-    it("approach1",()=>{
+    it.only("approach1",()=>{
 
         cy.visit("https://the-internet.herokuapp.com/iframe")
         cy.wait(10000)
         let iframe = cy.get("#mce_0_ifr")
         .its('0.contentDocument.body').should("be.visible").then(cy.wrap)
 
-        let text=iframe.clear().type("I here inside the Iframe").then(cy.wrap)
-
-        text.type("{cmd+a}")
+        iframe.clear().type("I here inside the Iframe {cmd+a}")
 
         cy.get("[aria-label='Bold']").click()
 
         cy.wait(5000)
+
+    })
+    it("approach1",()=>{
+
+        cy.visit("https://www.theguardian.com/international")
+       
 
     })
 
