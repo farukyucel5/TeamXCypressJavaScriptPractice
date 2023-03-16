81f985bcc526467f113e7364db10b2393677dae1
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-03-16 19:08:48 +0300

    Faruk 15.03.2023 updating

--- cypress/e2e/IFrames.cy.js
+++ cypress/e2e/IFrames.cy.js
@@ -2,28 +2,32 @@ describe("handling IFrames",() => {
 
     beforeEach(() => {
         cy.viewport(1280, 720)
+        cy.visit("https://the-internet.herokuapp.com/iframe")
     
       })
   
-    it.only("approach1",()=>{
+    it("approach1",()=>{
 
-        cy.visit("https://the-internet.herokuapp.com/iframe")
-        cy.wait(10000)
+        
         let iframe = cy.get("#mce_0_ifr")
         .its('0.contentDocument.body').should("be.visible").then(cy.wrap)
 
-        iframe.clear().type("I here inside the Iframe {cmd+a}")
+        iframe.clear()
+        iframe.type("I here inside the Iframe {cmd+a}")
 
         cy.get("[aria-label='Bold']").click()
 
         cy.wait(5000)
 
     })
-    it("approach1",()=>{
+    it.only("approach2",()=>{
 
-        cy.visit("https://www.theguardian.com/international")
-       
-
+      let iframe=cy.getIFrame('#mce_0_ifr')
+      iframe.clear()
+      iframe.type("I'm here inside the Iframe {cmd+a}")
+      cy.get("[aria-label='Bold']").click()
+      cy.wait(5000)
+        
     })
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,8 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+Cypress.Commands.add('getIFrame',(iframe)=>{
+
+    return cy.get(iframe).its('0.contentDocument.body').should('be.visible').then(cy.wrap);
+    
+})
