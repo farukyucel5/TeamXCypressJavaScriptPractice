199a4b199255b74458a2d8a391c329d522995949
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-03-22 23:55:53 +0300

    21.01.2023

---
+++ cypress/e2e/WebTables.cy.js
@@ -0,0 +1,15 @@
+describe("Webtables",()=>{
+  
+    beforeEach(() => {
+        cy.viewport(1280, 720)
+        cy.visit("https://trendlifebuy.com")
+        cy.xpath("(//i[@class='ti-close'])[2]").click()
+        
+    })
+
+
+
+
+
+
+})
